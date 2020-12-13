e425690e65ccfdfcd623261db70137ae8c49beea
clunven <cedrick.lunven@gmail.com>
2020-12-13 18:31:30 +0100

    OK

--- todobackend-springdata/src/main/java/com/datastax/workshop/TodobackendSpringdataApplication.java
+++ todobackend-springdata/src/main/java/com/datastax/workshop/TodobackendSpringdataApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.cassandra.CassandraAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.cassandra.CassandraDataAutoConfiguration;
 
-@SpringBootApplication(exclude = { CassandraDataAutoConfiguration.class, CassandraAutoConfiguration.class })
+@SpringBootApplication
+//(exclude = { CassandraDataAutoConfiguration.class, CassandraAutoConfiguration.class })
 public class TodobackendSpringdataApplication {
 
 	public static void main(String[] args) {
