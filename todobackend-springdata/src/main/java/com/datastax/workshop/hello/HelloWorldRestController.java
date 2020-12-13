7e234d086598a29e72f5dbd5687ec6b526beea1e
clunven <cedrick.lunven@gmail.com>
2020-12-13 21:04:35 +0100

    Get ready for part2

--- todobackend-springdata/src/main/java/com/datastax/workshop/hello/HelloWorldRestController.java
+++ todobackend-springdata/src/main/java/com/datastax/workshop/hello/HelloWorldRestController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloWorld {
+public class HelloWorldRestController {
 
     @RequestMapping("/")
     public String hello() {
